a103b03c4a841cbb206ad2c1b94df9b57ddfbbb3
Marko Kermiciev <kermicievmarko@gmail.com>
2022-12-19 23:08:32 +0100

    implement first two features in mobile menu

---
+++ script.js
@@ -0,0 +1,14 @@
+const bar = document.querySelector(".fa-bars");
+const logo = document.querySelector(".logo");
+const menu = document.querySelector("#menu");
+
+bar.addEventListener("click", () => {
+  bar.classList.toggle("fa-xmark");
+  if (menu.style.display === "block" && logo.style.display === "none") {
+    menu.style.display = "none";
+    logo.style.display = "block";
+  } else {
+    menu.style.display = "block";
+    logo.style.display = "none";
+  }
+});
