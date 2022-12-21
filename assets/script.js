943c8977682f545717164c095313921eeaac9bf9
Marko Kermiciev <kermicievmarko@gmail.com>
2022-12-21 23:04:01 +0100

    Update of JS

--- assets/script.js
+++ assets/script.js
@@ -32,7 +32,7 @@ const project_popup = [
     skillList: ["CANOPY", "Back End Dev", 2015],
     desktopImage: "Images/popup_window_desktopImg.png",
     mobileImage: "Images/popup_window_mobImg.png",
-    discription:
+    description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry is standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     mobileTech: ["html", "css", "javascript"],
     desktopTech: ["html", "css", "javascript", "github", "ruby", "bootstrap"],
@@ -45,7 +45,7 @@ project_popup.name = "Tonic";
 project_popup.skillList = ["CANOPY", "Back End Dev", 2015];
 project_popup.desktopImage = "Images/popup_window_desktopImg.png";
 project_popup.mobileImage = "Images/popup_window_mobImg.png";
-project_popup.discription =
+project_popup.description =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry is standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent";
 project_popup.mobileTech = ["html", "css", "javascript"];
 project_popup.desktopTech = [
@@ -74,7 +74,7 @@ function openModal() {
   detail.style.display = "block";
   detailTitle.textContent = project_popup.name;
   detailText.textContent = project_popup.description;
-  liveLink.href = project_popup.liveLink;
+  detailSkill.textContent = liveLink.href = project_popup.liveLink;
   sourceLink.href = project_popup.sourceLink;
   if (screenWidth < 768) {
     img.setAttribute("src", project_popup.mobileImage);
